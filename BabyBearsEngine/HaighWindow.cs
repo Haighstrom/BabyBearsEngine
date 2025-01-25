51e7118f0907b49345c0e8a70ba2e673c91022df
Haighstrom <simon.haigh@gmail.com>
2025-01-25 18:44:54 +0000

    Got Hello Triangle working in some shit way

--- BabyBearsEngine/HaighWindow.cs
+++ BabyBearsEngine/HaighWindow.cs
@@ -1,8 +1,67 @@
-﻿using OpenTK.Windowing.Desktop;
+﻿using BabyBearsEngine.Source.Graphics;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
 
 namespace BabyBearsEngine;
 
 public class HaighWindow(int width, int height, string title) 
     : GameWindow(GameWindowSettings.Default, new NativeWindowSettings() { ClientSize = (width, height), Title = title })
 {
+    private readonly float[] _vertices = {
+    -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+     0.5f, -0.5f, 0.0f, //Bottom-right vertex
+     0.0f,  0.5f, 0.0f  //Top vertex
+    };
+    int _vertexBufferObject;
+    Shader _shader = null!;
+    int _vertexArrayObject;
+
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+
+        GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+        _vertexBufferObject = GL.GenBuffer();
+        _shader = new Shader("shader.vert", "shader.frag");
+        _vertexArrayObject = GL.GenVertexArray();
+
+        GL.BindVertexArray(_vertexArrayObject);
+        GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+        GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+        GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+        GL.EnableVertexAttribArray(0);
+
+        GL.BindVertexArray(_vertexArrayObject);
+        GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+        GL.EnableVertexAttribArray(0);
+    }
+
+    protected override void OnUnload()
+    {
+        base.OnUnload();
+
+        _shader.Dispose();
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs args)
+    {
+        base.OnRenderFrame(args);
+
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+
+        _shader.Use();
+        GL.BindVertexArray(_vertexArrayObject);
+        GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+        SwapBuffers();
+    }
+
+    protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
+    {
+        base.OnFramebufferResize(e);
+
+        GL.Viewport(0, 0, e.Width, e.Height);
+    }
 }
---
+++ BabyBearsEngine/Source/Graphics/Shader.cs
@@ -0,0 +1,94 @@
+﻿using System.IO;
+using OpenTK.Graphics.OpenGL4;
+
+namespace BabyBearsEngine.Source.Graphics;
+
+public class Shader : IDisposable
+{
+    private readonly int _handle;
+
+    public Shader(string vertexPath, string fragmentPath)
+    {
+        int vertexShader;
+        int fragmentShader;
+
+        string vertexShaderSource = File.ReadAllText(vertexPath);
+
+        string fragmentShaderSource = File.ReadAllText(fragmentPath);
+
+        vertexShader = GL.CreateShader(ShaderType.VertexShader);
+        GL.ShaderSource(vertexShader, vertexShaderSource);
+
+        fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+        GL.ShaderSource(fragmentShader, fragmentShaderSource);
+
+        GL.CompileShader(vertexShader);
+
+        GL.GetShader(vertexShader, ShaderParameter.CompileStatus, out int success);
+        if (success == 0)
+        {
+            string infoLog = GL.GetShaderInfoLog(vertexShader);
+            Console.WriteLine(infoLog);
+        }
+
+        GL.CompileShader(fragmentShader);
+
+        GL.GetShader(fragmentShader, ShaderParameter.CompileStatus, out int success2);
+        if (success2 == 0)
+        {
+            string infoLog = GL.GetShaderInfoLog(fragmentShader);
+            Console.WriteLine(infoLog);
+        }
+
+        _handle = GL.CreateProgram();
+
+        GL.AttachShader(_handle, vertexShader);
+        GL.AttachShader(_handle, fragmentShader);
+
+        GL.LinkProgram(_handle);
+
+        GL.GetProgram(_handle, GetProgramParameterName.LinkStatus, out int success3);
+        if (success3 == 0)
+        {
+            string infoLog = GL.GetProgramInfoLog(_handle);
+            Console.WriteLine(infoLog);
+        }
+        GL.DetachShader(_handle, vertexShader);
+        GL.DetachShader(_handle, fragmentShader);
+        GL.DeleteShader(fragmentShader);
+        GL.DeleteShader(vertexShader);
+    }
+
+    public void Use()
+    {
+        GL.UseProgram(_handle);
+    }
+
+    #region IDisposable
+    private bool disposedValue = false;
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            GL.DeleteProgram(_handle);
+
+            disposedValue = true;
+        }
+    }
+
+    ~Shader()
+    {
+        if (disposedValue == false)
+        {
+            Console.WriteLine("GPU Resource leak! Did you forget to call Dispose()?");
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+    #endregion
+}
