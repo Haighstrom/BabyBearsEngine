1c4fe97fef263ca6423d10c684ff7c911f7bf5da
Haighstrom <simon.haigh@gmail.com>
2025-02-01 20:19:16 +0000

    simplified ctr of shader

--- BabyBearsEngine/Source/Graphics/Shaders/DefaultShaderProgram.cs
+++ BabyBearsEngine/Source/Graphics/Shaders/DefaultShaderProgram.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using BabyBearsEngine.Source.Graphics.Components;
 using OpenTK.Graphics.OpenGL4;
 using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
@@ -9,55 +10,19 @@ namespace BabyBearsEngine.Source.Graphics.Shaders;
 public class DefaultShaderProgram : IShaderProgram
 {
     private bool _disposed = false;
+    private readonly int _windowSizeLocation;
 
     public DefaultShaderProgram(GameWindow window)
     {
-        int vertexShader;
-        int fragmentShader;
-        string vertexSource = File.ReadAllText("Assets/Shaders/shader.vert");
-        string fragmentSource = File.ReadAllText("Assets/Shaders/shader.frag");
+        string vsSource = File.ReadAllText("Assets/Shaders/shader.vert");
+        int vertexShader = OpenGLHelper.CreateShader(vsSource, ShaderType.VertexShader);
 
-        vertexShader = GL.CreateShader(ShaderType.VertexShader);
-        GL.ShaderSource(vertexShader, vertexSource);
+        string fsSource = File.ReadAllText("Assets/Shaders/shader.frag");
+        int fragmentShader = OpenGLHelper.CreateShader(fsSource, ShaderType.FragmentShader);
 
-        fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
-        GL.ShaderSource(fragmentShader, fragmentSource);
+        Handle = OpenGLHelper.CreateShaderProgram(vertexShader, fragmentShader);
 
-        GL.CompileShader(vertexShader);
-
-        GL.GetShader(vertexShader, ShaderParameter.CompileStatus, out var success);
-        if (success == 0)
-        {
-            var infoLog = GL.GetShaderInfoLog(vertexShader);
-            Console.WriteLine(infoLog);
-        }
-
-        GL.CompileShader(fragmentShader);
-
-        GL.GetShader(fragmentShader, ShaderParameter.CompileStatus, out var success2);
-        if (success2 == 0)
-        {
-            var infoLog = GL.GetShaderInfoLog(fragmentShader);
-            Console.WriteLine(infoLog);
-        }
-
-        Handle = GL.CreateProgram();
-
-        GL.AttachShader(Handle, vertexShader);
-        GL.AttachShader(Handle, fragmentShader);
-
-        GL.LinkProgram(Handle);
-
-        GL.GetProgram(Handle, GetProgramParameterName.LinkStatus, out var success3);
-        if (success3 == 0)
-        {
-            var infoLog = GL.GetProgramInfoLog(Handle);
-            Console.WriteLine(infoLog);
-        }
-        GL.DetachShader(Handle, vertexShader);
-        GL.DetachShader(Handle, fragmentShader);
-        GL.DeleteShader(fragmentShader);
-        GL.DeleteShader(vertexShader);
+        _windowSizeLocation = GL.GetUniformLocation(Handle, "WindowSize");
 
         window.Resize += Window_Resize;
     }
@@ -67,8 +32,7 @@ public class DefaultShaderProgram : IShaderProgram
     private void Window_Resize(ResizeEventArgs args)
     {
         Bind();
-        var windowSizeLocation = GL.GetUniformLocation(Handle, "WindowSize");
-        GL.Uniform2(windowSizeLocation, new Vector2(args.Width, args.Height));
+        GL.Uniform2(_windowSizeLocation, new Vector2(args.Width, args.Height));
     }
 
     public void Bind()
