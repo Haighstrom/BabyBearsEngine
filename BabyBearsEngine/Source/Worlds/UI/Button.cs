98200ae0ce0110d73244fe8dabf4809f69f00ea9
Simon Haigh <simon.haigh@gmail.com>
2026-04-12 20:05:23 +0100

    updated Button to auto colour

--- BabyBearsEngine/Source/Worlds/UI/Button.cs
+++ BabyBearsEngine/Source/Worlds/UI/Button.cs
@@ -1,89 +1,46 @@
 ﻿using BabyBearsEngine.Graphics;
-using BabyBearsEngine.Input;
 using BabyBearsEngine.Source.Geometry;
 using BabyBearsEngine.Source.Rendering.Graphics.Text;
-using BabyBearsEngine.Source.Worlds;
 
 namespace BabyBearsEngine.Worlds.UI;
 
-public class Button : IEntity, IClickable
+public class Button(int x, int y, int width, int height, Colour colour, string textToDisplay = "") 
+    : ClickableEntity(x, y, width, height)
 {
-    private const double HoverDelaySeconds = 0.5; // tweak as needed
+    private readonly ColouredRectangle _rectangle = new(colour, x, y, width, height);
+    private readonly TextImage _textImage = new(new FontDefinition("Times New Roman", 16), textToDisplay, Colour.Black, x, y, width, height);
 
-    private readonly ColouredRectangle _graphic;
-    private readonly TextImage _textImage;
-    private readonly ClickController _buttonHandler;
-    private readonly int _x;
-    private readonly int _y;
-    private readonly int _width;
-    private readonly int _height;
-
-    public Button(int x, int y, int width, int height, Colour colour, string textToDisplay = "")
+    public override void Render(ref Matrix3 projection, ref Matrix3 modelView)
     {
-        _x = x;
-        _y = y;
-        _width = width;
-        _height = height;
-        _graphic = new(colour, x, y, width, height);
-        _textImage = new(new FontDefinition("Times New Roman", 16), textToDisplay, Colour.Black, x, y, width, height);
-        _buttonHandler = new ClickController(this, HoverDelaySeconds);
-    }
-
-    private bool MouseOver => 
-        Mouse.ClientX >= _x && Mouse.ClientX < _x + _width &&
-        Mouse.ClientY >= _y && Mouse.ClientY < _y + _height;
-
-    public void Update(double elapsed)
-    {
-        if (MouseOver)
-        {
-            MouseSolver.RegisterMouseOver(_buttonHandler);
-        }
-    }
-
-    // --- Interface Implementation (The Bridge) ---
-    void IClickable.TriggerLeftPressed() => OnLeftPressed(); 
-    void IClickable.TriggerLeftReleased() => OnLeftReleased(); 
-    void IClickable.TriggerMouseEntered() => OnMouseEntered(); 
-    void IClickable.TriggerMouseExited() => OnMouseExited(); 
-    void IClickable.TriggerHover() => OnMouseHovered(); 
-    void IClickable.TriggerCancelHover() => OnStopMouseHovered();
-
-    protected virtual void OnStopMouseHovered()
-    {
-    }
-
-    protected virtual void OnLeftReleased() 
-    {
-        LeftClicked?.Invoke(this, EventArgs.Empty);
-    }
-    protected virtual void OnLeftPressed() 
-    {
-        LeftPressed?.Invoke(this, EventArgs.Empty);
-    }
-    protected virtual void OnMouseEntered() 
-    {
-        MouseEntered?.Invoke(this, EventArgs.Empty);
-    }
-    protected virtual void OnMouseExited() 
-    {
-        MouseExited?.Invoke(this, EventArgs.Empty);
-    }
-    protected virtual void OnMouseHovered() 
-    {
-        MouseHovered?.Invoke(this, EventArgs.Empty);
-    }
-
-    public event EventHandler? LeftClicked;
-    public event EventHandler? LeftPressed;
-    public event EventHandler? LeftReleased;
-    public event EventHandler? MouseEntered;
-    public event EventHandler? MouseExited;
-    public event EventHandler? MouseHovered;
-
-    public void Render(ref Matrix3 projection, ref Matrix3 modelView)
-    {
-        _graphic.Render(ref projection, ref modelView);
+        _rectangle.Render(ref projection, ref modelView);
         _textImage.Render(ref projection, ref modelView);
+    }
+
+    protected override void OnMouseEntered()
+    {
+        base.OnMouseEntered();
+
+        _rectangle.Colour = Colour.LightGray;
+    }
+
+    protected override void OnMouseExited()
+    {
+        base.OnMouseExited();
+
+        _rectangle.Colour = colour;
+    }
+
+    protected override void OnLeftPressed()
+    {
+        base.OnLeftPressed();
+
+        _rectangle.Colour = Colour.LightBlue;
+    }
+
+    protected override void OnLeftReleased()
+    {
+        base.OnLeftReleased();
+
+        _rectangle.Colour = Colour.LightGray;
     }
 }
