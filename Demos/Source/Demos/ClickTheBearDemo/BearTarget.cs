e01d828658e860444c7e362f54ad086bc29622cc
Simon Haigh <simon.haigh@gmail.com>
2026-04-26 22:08:07 +0100

    built click the bear demo

---
+++ Demos/Source/Demos/ClickTheBearDemo/BearTarget.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using BabyBearsEngine.Worlds.Graphics;
+
+namespace BabyBearsEngine.Demos.Source.Demos.ClickTheBearDemo;
+
+internal class BearTarget : Entity
+{
+    private const double LifeSpan = 3.0;
+    private const double FadeStartAt = 1.0;
+
+    private readonly Image _image;
+    private double _lifeRemaining = LifeSpan;
+    private bool _done;
+
+    public event EventHandler? BearClicked;
+    public event EventHandler? BearExpired;
+
+    public BearTarget(int x, int y) : base(x, y, 60, 60, clickable: true)
+    {
+        _image = new Image(Textures.CreateFromFile("Assets/SpinnableBear.png"), 0, 0, 60, 60);
+        Add(_image);
+    }
+
+    public override void Update(double elapsed)
+    {
+        base.Update(elapsed);
+
+        if (_done)
+        {
+            return;
+        }
+
+        _lifeRemaining -= elapsed;
+
+        if (_lifeRemaining <= FadeStartAt)
+        {
+            _image.Alpha = (float)Math.Max(0, _lifeRemaining / FadeStartAt);
+        }
+
+        if (_lifeRemaining <= 0)
+        {
+            _done = true;
+            BearExpired?.Invoke(this, EventArgs.Empty);
+            Remove();
+        }
+    }
+
+    protected override void OnLeftReleased()
+    {
+        if (_done)
+        {
+            return;
+        }
+
+        _done = true;
+        BearClicked?.Invoke(this, EventArgs.Empty);
+        Remove();
+    }
+}
--- Demos/Source/Demos/ClickTheBearDemo/ClickTheBearDemoWorld.cs
+++ Demos/Source/Demos/ClickTheBearDemo/ClickTheBearDemoWorld.cs
@@ -1,13 +1,59 @@
-using System;
-using BabyBearsEngine.Demos.Source;
+﻿using System;
+using BabyBearsEngine.Worlds.Graphics;
 
 namespace BabyBearsEngine.Demos.Source.Demos.ClickTheBearDemo;
 
 internal class ClickTheBearDemoWorld : DemoWorld
 {
+    private const double SpawnInterval = 1.5;
+    private const int BearSize = 60;
+    private const int ScoreIconSize = 24;
+    private const int ScoreIconGap = 4;
+    private const int ScoreMargin = 8;
+
+    private double _spawnTimer = 0;
+    private int _scoreCount = 0;
+
     public override string Name => "Click The Bear";
 
     public ClickTheBearDemoWorld(Func<World> menuWorldFactory) : base(menuWorldFactory)
     {
+        BackgroundColour = new Colour(34, 139, 34);
+    }
+
+    public override void Update(double elapsed)
+    {
+        base.Update(elapsed);
+
+        _spawnTimer -= elapsed;
+        if (_spawnTimer <= 0)
+        {
+            SpawnBear();
+            _spawnTimer = SpawnInterval;
+        }
+    }
+
+    private void SpawnBear()
+    {
+        int scoreBarBottom = ScoreMargin + ScoreIconSize + ScoreMargin;
+        int margin = 20;
+        int x = Randomisation.Rand(margin, Window.Width - BearSize - margin);
+        int y = Randomisation.Rand(scoreBarBottom + margin, Window.Height - BearSize - margin);
+
+        var bear = new BearTarget(x, y);
+        bear.BearClicked += (_, _) => AddScoreIcon();
+        Add(bear);
+    }
+
+    private void AddScoreIcon()
+    {
+        int iconsPerRow = (Window.Width - ScoreMargin * 2) / (ScoreIconSize + ScoreIconGap);
+        int col = _scoreCount % iconsPerRow;
+        int row = _scoreCount / iconsPerRow;
+        float x = ScoreMargin + col * (ScoreIconSize + ScoreIconGap);
+        float y = ScoreMargin + row * (ScoreIconSize + ScoreIconGap);
+
+        Add(new Image(Textures.CreateFromFile("Assets/SpinnableBear.png"), x, y, ScoreIconSize, ScoreIconSize));
+        _scoreCount++;
     }
 }
