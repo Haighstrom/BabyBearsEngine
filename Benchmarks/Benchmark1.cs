2105efd7fcf1300f8acda9722f112691422e7475
Simon Haigh <simon.haigh@gmail.com>
2026-04-26 12:13:40 +0100

    added a benchmark project

---
+++ Benchmarks/Benchmark1.cs
@@ -0,0 +1,12 @@
+using BenchmarkDotNet.Attributes;
+
+namespace BabyBearsEngine.Benchmarks;
+
+[MemoryDiagnoser]
+public class Benchmark1
+{
+    [Benchmark]
+    public void Benchmark()
+    {
+    }
+}
