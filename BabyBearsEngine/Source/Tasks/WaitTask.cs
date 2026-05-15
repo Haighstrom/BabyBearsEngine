561ce6e2c0b26c34569ad93a6e5a054dbdfc243f
Simon Haigh <simon.haigh@gmail.com>
2026-05-15 22:10:37 +0100

    add WaitTask, closes #15

---
+++ BabyBearsEngine/Source/Tasks/WaitTask.cs
@@ -0,0 +1,26 @@
+namespace BabyBearsEngine.Tasks;
+
+/// <summary>
+/// A task that waits for a fixed duration then signals completion.
+/// </summary>
+public class WaitTask(double duration) : Task
+{
+    private double _elapsed = 0.0;
+
+    /// <inheritdoc/>
+    public override bool IsComplete => _elapsed >= duration;
+
+    /// <inheritdoc/>
+    public override void Reset()
+    {
+        base.Reset();
+        _elapsed = 0.0;
+    }
+
+    /// <inheritdoc/>
+    public override void Update(double elapsed)
+    {
+        base.Update(elapsed);
+        _elapsed += elapsed;
+    }
+}
